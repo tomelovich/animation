2bbfdc51065268297ad0a37fbe5de7c716c6c156
tomelovich <tomelovichs@gmail.com>
2022-06-21 16:17:45 +0300

    Не сделано 2 и несколько доп. заданий

---
+++ 29/29/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("29")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("29")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("8ab682f4-328a-49e2-9a20-86e061512438")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 29/4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4
+{
+    public partial class Form1 : Form
+    {
+        const float PI = (float)Math.PI;
+        Timer tmr = new Timer() { Interval = 50 };
+        GraphicsPath sinusoid;
+        //PointF pt;
+        int n;
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Click += button1_Click;
+            pictureBox1.Paint += pictureBox1_Paint;
+            tmr.Tick += tmr_Tick;
+            sinusoid = new GraphicsPath();
+        }
+
+        void tmr_Tick(object sender, EventArgs e)
+        {
+            if (++n >= sinusoid.PointCount) n = 0;
+            pictureBox1.Refresh();
+        }
+
+        void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (sinusoid.PointCount == 0) return;
+            PointF pt = sinusoid.PathPoints[n];
+            e.Graphics.ScaleTransform(1, -1);
+            e.Graphics.TranslateTransform(5, -pictureBox1.Height / 2);
+            e.Graphics.DrawPath(Pens.Red, sinusoid);
+
+            e.Graphics.FillEllipse(Brushes.Blue, RectangleF.FromLTRB(pt.X - 15, pt.Y - 15, pt.X + 15, pt.Y + 15));
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            n = 0;
+            CreateSinusoid(sinusoid, 10f * PI, pictureBox1.ClientSize);
+            if (tmr.Enabled)
+                tmr.Stop();
+            else
+                tmr.Start();
+        }
+        private void CreateSinusoid(GraphicsPath gp, float MaxX, Size size)
+        {
+            gp.Reset();
+            PointF[] points = new PointF[1] { PointF.Empty };
+            for (float i = 0; i <= MaxX; i += 0.2f)
+            {
+                points[points.GetUpperBound(0)] = new PointF(i, (float)Math.Sin(i));
+                Array.Resize<PointF>(ref points, points.Length + 1);
+            }
+            Array.Resize<PointF>(ref points, points.Length - 1);
+            gp.AddCurve(points);
+            Matrix m = new Matrix();
+            m.Scale((float)(size.Width / MaxX), 0.4f * size.Height);
+            gp.Transform(m);
+        }
+    }
+}
